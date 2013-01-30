9c89c271c2b9767457d7c773874fc54281c8e53d
Daniel Pereira <dtvp@cesar.org.br>
2013-01-30 13:16:20 -0200

    Added stubs.

--- projs/ServoByMKI123/src/main.c
+++ projs/ServoByMKI123/src/main.c
@@ -75,32 +75,15 @@ int main(void)
 
 
 /**
- * @brief	Configure MEMS operation
+ * @brief	Configure LSM330D MEMS operation
  * @param	None
  * @retval	None
  */
 void MEMS_Config(void)
 {
-  LIS302DL_InitTypeDef  		LIS302DL_InitStruct;
-  LIS302DL_FilterConfigTypeDef	LIS302DL_FilterStruct;
-  
-  /* Set configuration of LIS302DL*/
-  LIS302DL_InitStruct.Power_Mode = LIS302DL_LOWPOWERMODE_ACTIVE;
-  LIS302DL_InitStruct.Output_DataRate = LIS302DL_DATARATE_100;
-  LIS302DL_InitStruct.Axes_Enable = LIS302DL_X_ENABLE | LIS302DL_Y_ENABLE;
-  LIS302DL_InitStruct.Full_Scale = LIS302DL_FULLSCALE_2_3;
-  LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
-  LIS302DL_Init(&LIS302DL_InitStruct);
 
-  LIS302DL_FilterStruct.HighPassFilter_Data_Selection = LIS302DL_FILTEREDDATASELECTION_OUTPUTREGISTER;
-  LIS302DL_FilterStruct.HighPassFilter_CutOff_Frequency = LIS302DL_HIGHPASSFILTER_LEVEL_1;
-  LIS302DL_FilterStruct.HighPassFilter_Interrupt = LIS302DL_HIGHPASSFILTERINTERRUPT_OFF;
-  LIS302DL_FilterConfig(&LIS302DL_FilterStruct);
 
-  LIS302DL_Read(Buffer, LIS302DL_OUT_X_ADDR, 4);
-                  
-  XOffset = Buffer[0];
-  YOffset = Buffer[2];
+
 }
 
 
